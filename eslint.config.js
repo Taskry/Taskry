ce781fd1281d7c41605676e343cc8cf10d8df236
Won Chan LEE <gxpk2978@naver.com>
2025-11-05 17:02:35 +0900

    [chore]:add eslint + prettier team config

--- eslint.config.js
+++ eslint.config.js
@@ -2,6 +2,8 @@ import js from '@eslint/js'
 import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
+import prettier from 'eslint-plugin-prettier'
+import prettierConfig from 'eslint-config-prettier'
 import { defineConfig, globalIgnores } from 'eslint/config'
 
 export default defineConfig([
@@ -12,7 +14,14 @@ export default defineConfig([
       js.configs.recommended,
       reactHooks.configs['recommended-latest'],
       reactRefresh.configs.vite,
+      prettierConfig
     ],
+    plugins: {
+      reactHooks,
+      reactRefresh,
+      prettier
+    },
+
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
@@ -24,6 +33,7 @@ export default defineConfig([
     },
     rules: {
       'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
+      'prettier/prettier': 'error',
     },
   },
 ])
